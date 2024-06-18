4ff7c25304e091ff9907ce3a6cf4684bab6ee8fe
Igor Nicoletti <igornicoletti@outlook.com>
2024-06-18 17:42:03 -0300

    api vercel

--- api/server.js
+++ api/server.js
@@ -5,12 +5,8 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
-server.use(jsonServer.rewriter({
-  '/api/*': '/$1',
-  '/blog/:resource/:id/show': '/:resource/:id'
-}))
+server.use(jsonServer.rewriter({ '/api/*': '/$1' }))
 server.use(router)
-
-server.listen(3000, () => { console.log('JSON Server is running') })
+server.listen(3000, () => console.log('JSON Server is running'))
 
 module.exports = server
